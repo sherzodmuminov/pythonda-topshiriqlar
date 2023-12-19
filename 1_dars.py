c96e7e32b7bb081c83c2db0a9f02c0b427757880
sherzodmuminov <153056708+sherzodmuminov@users.noreply.github.com>
2023-12-19 14:34:02 +0300

    yangi dars yukladim
    1-dars haqida tushunchalar

---
+++ 1_dars.py
@@ -0,0 +1,58 @@
+#print('Salom yxshinmisiz?')
+
+ #string (satr) "" '' """ """ str() repr()
+# int (butun son) = 24 int()
+# float (qoldiq son) = 34.5 float()
+# boolean (mantiqiy ma`lumot turi) = True/False bool()
+# list (ro`yxat o'\garuvchan) = [], list()
+# tuple (ro`yxat o'zgarmaydi) = (), tuple()
+# dictionary (ro'yxat juft-juft) = {}, dict()
+# set (ro'yxat, tartibsiz) = {}, set()
+# none = (bo'shliq) = None
+
+ism = 'Sherzodbek'
+# bosh xarf bilan yozilamydi
+# raqam bilan boshlanmaydi
+# orada joy tashlanmaydi
+# shart belgilar ishtrok etmaydi (_)
+# kalit so'zlardan foydalanilmaydi
+# lotin tilida yozilishi kerak
+
+#print(ism)
+#snake case
+dadam_ism = 'Muhammadjon'
+
+#Camel Case
+dadamIsm = 'Muhammadjon'
+
+#Pascal case
+DadamIsm = 'Muhammadjon'
+#print(dadamIsm)
+#print(dadam_ism)
+#print(DadamIsm) 
+ISM = 'Muhammadjon'
+
+raqam:int = 24
+
+a,b,c = 10.2, 10, 'a'
+
+
+
+
+# +, -, *, /, //, %, **
+# print(round(5.9))
+# print(pow(2, 4, 3))
+# print(divmod(10,3))
+# print(abs(10))
+
+# 1
+#year = int(input("nechanchi yili tug'ilgansiz? "))
+
+# 2
+#year = input("nechanchi yili tug'ilgansiz? ")
+# year_int = int(year)
+# 3
+
+year = input("nechanchi yili tug'ilgansiz? ")
+#year = int(year)
+print(type(year))
